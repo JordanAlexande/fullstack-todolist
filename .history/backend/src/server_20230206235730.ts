25b55d626ef641691cdc747966a5dbafb3130835
Jordan Huaman <83663050+JordanAlexande@users.noreply.github.com>
2023-02-06 23:58:58 -0500

    config dotenv

--- .history/backend/src/server_20230206235730.ts
+++ .history/backend/src/server_20230206235730.ts
@@ -2,7 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 
 app.get('/', (req, res) => {
